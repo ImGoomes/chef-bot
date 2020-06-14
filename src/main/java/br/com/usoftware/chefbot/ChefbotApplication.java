fdae406eebde3968398c924e793a1b378640a016
Bruno Mendes <bruno.mendes51@hotmail.com>
2020-06-13 21:05:21 -0300

    init

---
+++ src/main/java/br/com/usoftware/chefbot/ChefbotApplication.java
@@ -0,0 +1,13 @@
+package br.com.usoftware.chefbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/usoftware/chefbot/ChefbotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.usoftware.chefbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
